b184599cf8d60b4ca1424807afbc5a2dbe176a72
solo12345689 <sarthakhasija01@gmail.com>
2025-11-24 21:33:50 +0530

    add

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from .api import router as api_router
+from api import router as api_router
 
 app = FastAPI(title="MovieBox Web App", description="API for MovieBox Web App")
 
